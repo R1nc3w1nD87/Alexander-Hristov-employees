5b1a6a08a151a287e2b907238c4aabbb17e98330
R1nc3w1nD87 <pianoman87@abv.bg>
2021-11-10 19:30:37 +0200

    Updated OOP structure of code.

--- src/main/java/Employees.java
+++ src/main/java/Employees.java
@@ -5,64 +5,79 @@ import java.util.*;
 
 public class Employees {
     public static void main(String[] args) {
-        List<int[]> employeeProjects = new ArrayList<>();
-        try {
 
+        List<int[]> inputRecords = new ArrayList<>();
+
+        try {
             FileInputStream inputStream = new FileInputStream("src/main/InputFile/Input.txt");
             Scanner scan = new Scanner(inputStream);
-            while (scan.hasNext()){
-                String[] line = scan.nextLine().split(",\\s+");
-                if (line[3].equals("NULL")){
-                    line[3] = LocalDate.now().toString();
-                }
-                LocalDate dateFrom = LocalDate.parse(line[2]);
-                LocalDate dateTo = LocalDate.parse(line[3]);
-                int currentDateSum = dateTo.compareTo(dateFrom);
-                int currentEmployee = Integer.parseInt(line[0]);
-                int currentProject = Integer.parseInt(line[1]);
-                int[] currentEmpProj = new int[3];
-                currentEmpProj[0] = currentEmployee;
-                currentEmpProj[1] = currentProject;
-                currentEmpProj[2] = currentDateSum;
-
-                employeeProjects.add(currentEmpProj);
-
-            }
+            parseInputData(inputRecords, scan);
 
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
-        int[] longestTeamUpEmployees = new int[2];
-        Map<int[], Integer> totalProjectsMaxSums = new HashMap<>();
-        for (int i = 0; i < employeeProjects.size(); i++) {
-            int currentEmployee = employeeProjects.get(i)[0];
-            int currentProject = employeeProjects.get(i)[1];
-            int currentDateSum = employeeProjects.get(i)[2];
+        Credentials crds = new Credentials();
+
+        Map<Credentials, Integer> projectData = new HashMap<>();
+        mapData(inputRecords, crds, projectData);
+        int[] oldestPair = findMax(projectData);
+        System.out.println(Arrays.toString(oldestPair));
+    }
+
+    private static int[] findMax(Map<Credentials, Integer> projectData) {
+        int[] oldestPair = new int[2];
+        for (Map.Entry<Credentials, Integer> entry : projectData.entrySet()) {
+            int highestValue = 0;
+            Credentials currentKey = entry.getKey();
+            int currentValue = entry.getValue();
+            if (currentValue > highestValue){
+                oldestPair[0] = currentKey.getEmployeeOne();
+                oldestPair[1] = currentKey.getEmployeeTwo();
+                highestValue = currentValue;
+            }
+
+        }
+        return oldestPair;
+    }
+
+    private static void mapData(List<int[]> inputRecords,Credentials crds, Map<Credentials, Integer> totalProjectsMaxSums) {
+        for (int i = 0; i < inputRecords.size(); i++) {
+            int currentEmployee = inputRecords.get(i)[0];
+            int currentProject = inputRecords.get(i)[1];
+            int currentDateSum = inputRecords.get(i)[2];
             int longestProjectSum = 0;
-            for (int j = 0; j < employeeProjects.size(); j++) {
-                if (employeeProjects.get(j)[0] != currentEmployee
-                        && employeeProjects.get(j)[1] == currentProject){
-                    longestTeamUpEmployees[0] = currentEmployee;
-                    longestTeamUpEmployees[1] = employeeProjects.get(j)[0];
-                    int  currentLongestSum = currentDateSum + employeeProjects.get(j)[2];
-                    totalProjectsMaxSums.put(longestTeamUpEmployees, currentLongestSum);
+            for (int[] inputRecord : inputRecords) {
+                if (inputRecord[0] != currentEmployee
+                        && inputRecord[1] == currentProject) {
+                    crds.setEmployeeOne(currentEmployee);
+                    crds.setEmployeeTwo(inputRecord[0]);
+                    int currentLongestSum = currentDateSum + inputRecord[2];
+                    totalProjectsMaxSums.put(crds, currentLongestSum);
                     longestProjectSum += currentLongestSum;
                 }
             }
 
         }
-        int[] oldestPair = new int[2];
-        for (Map.Entry<int[], Integer> entry : totalProjectsMaxSums.entrySet()) {
-            int highestValue = 0;
-            int[] currentKey = entry.getKey();
-            int currentValue = entry.getValue();
-            if (currentValue > highestValue){
-                oldestPair[0] = currentKey[0];
-                oldestPair[1] = currentKey[1];
-                highestValue = currentValue;
+    }
+
+    private static void parseInputData(List<int[]> employeeProjects, Scanner scan) {
+        while (scan.hasNext()){
+            String[] line = scan.nextLine().split(",\\s+");
+            if ("NULL".equals(line[3])){
+                line[3] = LocalDate.now().toString();
             }
+            LocalDate dateFrom = LocalDate.parse(line[2]);
+            LocalDate dateTo = LocalDate.parse(line[3]);
+            int currentDateSum = dateTo.compareTo(dateFrom);
+            int currentEmployee = Integer.parseInt(line[0]);
+            int currentProject = Integer.parseInt(line[1]);
+            int[] currentEmpProj = new int[3];
+            currentEmpProj[0] = currentEmployee;
+            currentEmpProj[1] = currentProject;
+            currentEmpProj[2] = currentDateSum;
+
+            employeeProjects.add(currentEmpProj);
 
         }
-        System.out.println(Arrays.toString(oldestPair));
     }
 }
